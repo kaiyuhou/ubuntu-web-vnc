d18af169f21c825d8eaea395949a10d3db99d6fd
kaiyuhou <kaiyhou@gmail.com>
2021-09-09 15:00:27 -0500

    delete unnecessary content

--- src/ubuntu/install/libnss_wrapper.sh
+++ src/ubuntu/install/libnss_wrapper.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 ### every exit != 0 fails the script
+
+#  http://manpages.ubuntu.com/manpages/bionic/man1/nss_wrapper.1.html
+#  There are projects which provide daemons needing to be able to create, modify and delete
+#       Unix users. Or just switch user ids to interact with the system e.g. a user space file
+#       server. To be able to test that you need the privilege to modify the passwd and groups
+#       file. With nss_wrapper it is possible to define your own passwd and groups file which will
+#       be used by software to act correctly while under test.
+
 set -e
 
 echo "Install nss-wrapper to be able to execute image as non-root user"
